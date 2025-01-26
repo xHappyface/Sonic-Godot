d4214d851fff47a615ff4db3399d2c61f46b23bf
Rodrigo Ponce <96100905+xHappyface@users.noreply.github.com>
2025-01-26 07:48:59 -0500

    improved physics and animation system. added sound effects. charged spindash complete.

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,11 @@ class_name Player
 
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
+
+@onready var _sfx_jump: AudioStreamWAV = preload("res://assets/sounds/sound effects/Sonic_Jump.wav")
+@onready var _sfx_brake: AudioStreamWAV = preload("res://assets/sounds/sound effects/Sonic_Brake.wav")
+@onready var _sfx_charge: AudioStreamWAV = preload("res://assets/sounds/sound effects/Sonic_Charge.wav")
 
 const _GRAVITY: float = (96.0 / 256.0) * 60.0
 const _AIR_ACCELERATION: float = (24.0 / 256.0) * 60.0
@@ -21,6 +26,8 @@ var _control_lock: float = 0.0
 var _idling: float = 0.0
 var _is_jumping: bool = false
 var _spinrev: float = 0.0
+var _audio_buffer: float = 0.0
+var _is_spinning_out: bool = false
 
 var peeling_out: float = 0.0
 
@@ -29,11 +36,12 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	_control_lock = max(_control_lock - delta, 0.0)
-	var action_pressed: bool = Input.is_action_just_pressed("game_space")
+	var action_pressed: bool = Input.is_action_just_pressed("game_action")
 	var move_direction: Vector2 = Input.get_vector("game_left", "game_right", "game_down", "game_up")
 	var real_input: Vector2 = move_direction
 	var temp_spinrev: float = _spinrev
 	_spinrev = 0.0
+	_audio_buffer = max(_audio_buffer - delta, 0.0)
 	temp_spinrev = clamp(temp_spinrev - ((8.0 / 256.0) * delta), 0.0, 8.0)
 	if _control_lock > 0.0:
 		move_direction.x = 0.0
@@ -41,7 +49,7 @@ func _physics_process(delta: float) -> void:
 		move_direction.y = 0.0
 	if not is_on_floor():
 		velocity.y = min((velocity.y + _GRAVITY), _TOP_Y_SPEED)
-		if _is_jumping and not Input.is_action_pressed("game_space"):
+		if _is_jumping and not action_pressed:
 			if velocity.y < 0.0:
 				velocity.y = max(velocity.y, -4.0 * 60.0)
 		velocity.x = velocity.x + (move_direction.x * _AIR_ACCELERATION)
@@ -49,6 +57,8 @@ func _physics_process(delta: float) -> void:
 			velocity.x -= move_direction.x * _AIR_DRAG * delta
 	else:
 		_is_jumping = false
+		if _is_spinning_out and abs(velocity.x) <= 2.0 * 60.0:
+			_is_spinning_out = false
 		if velocity.x == 0.0 and move_direction.y < 0.0:
 			_spinrev = temp_spinrev
 		if temp_spinrev > 0.0 and move_direction.y >= 0.0:
@@ -57,14 +67,23 @@ func _physics_process(delta: float) -> void:
 				velocity.x = launch_speed * 60.0
 			else:
 				velocity.x = -launch_speed * 60.0
+			_is_spinning_out = true
 			_spinrev = 0.0
 		elif (action_pressed and real_input.y >= 0.0 and velocity.x == 0.0) or \
 		  (action_pressed and velocity.x != 0.0):
 			_is_jumping = true
 			velocity.y = max(velocity.y - _JUMP_FORCE, -_TOP_Y_SPEED)
+			if audio_player.stream != _sfx_jump:
+				audio_player.set_stream(_sfx_jump)
+			audio_player.play()
 		if action_pressed and velocity.x == 0.0:
 			_spinrev = min(temp_spinrev + 2.0, 8.0)
 			print(_spinrev)
+			if audio_player.stream != _sfx_charge:
+				audio_player.set_stream(_sfx_charge)
+			if _audio_buffer <= 0.0 or not audio_player.playing:
+				_audio_buffer = 0.175
+				audio_player.play()
 		elif real_input.x == 0.0:
 			if sign(velocity.x) > 0.0:
 				velocity.x = max(velocity.x - _FRICTION, 0.0)
@@ -83,7 +102,15 @@ func _physics_process(delta: float) -> void:
 			elif sign(move_direction.x) == sign(velocity.x) or sign(velocity.x) == 0.0:
 				velocity.x = clamp(velocity.x + (move_direction.x * _ACCELERATION), -_TOP_SPEED, _TOP_SPEED)
 			else:
-				velocity.x -= sign(velocity.x) * _DECELERATION
+				var original_sign: float = sign(velocity.x)
+				velocity.x -= original_sign * _DECELERATION
+				if sign(velocity.x) != original_sign:
+					velocity.x = -original_sign * 0.5
+				if abs(velocity.x) > 2.0 * 60.0:
+					if audio_player.stream != _sfx_brake:
+						audio_player.set_stream(_sfx_brake)
+					if not audio_player.playing:
+						audio_player.play()
 	_handle_animations(delta, move_direction)
 	move_and_slide()
 	if is_on_floor():
@@ -96,7 +123,7 @@ func _handle_animations(delta: float, move_direction: Vector2) -> void:
 	if (sign(move_direction.x) > 0.0 and sprite.flip_h) \
 	  or (sign(move_direction.x) < 0.0 and not sprite.flip_h):
 		sprite.flip_h = not sprite.flip_h
-	if _is_jumping:
+	if _is_jumping or _is_spinning_out:
 		_play_roll_animation()
 	else:
 		if not is_on_floor() and velocity.y >= _TOP_Y_SPEED / 2.0:
@@ -119,7 +146,12 @@ func _handle_animations(delta: float, move_direction: Vector2) -> void:
 			elif velocity.x != 0.0 and move_direction.y < 0.0:
 				_play_roll_animation()
 			elif velocity.x != 0.0 and (sign(move_direction.x) == sign(velocity.x) or move_direction.x == 0.0):
-				if (abs(velocity.x) > _TOP_SPEED or peeling_out >= 0.1) and anim_player.current_animation != "run2":
+				if sign(velocity.x) > 0.0:
+					sprite.flip_h = false
+				else:
+					sprite.flip_h = true
+				if (abs(velocity.x) > _TOP_SPEED or peeling_out >= 0.1) and \
+				  anim_player.current_animation != "run2":
 					anim_player.play("run2")
 				elif abs(velocity.x) >= _TOP_SPEED and \
 				  (anim_player.current_animation != "run1" or anim_player.current_animation != "run2"):
@@ -133,15 +165,19 @@ func _handle_animations(delta: float, move_direction: Vector2) -> void:
 						anim_player.play("run0_1")
 				else:
 					anim_player.play("run0")
-			elif anim_player.current_animation != "roll0" or anim_player.current_animation != "roll1":
+			elif abs(velocity.x) > 2.0 * 60.0 and \
+			  (anim_player.current_animation != "roll0" or anim_player.current_animation != "roll1"):
 				anim_player.play("brake")
 		elif is_on_floor() and move_direction.y != 0.0:
 			if move_direction.y > 0.0:
 				if anim_player.current_animation != "look_up":
 					anim_player.play("look_up")
 			elif _spinrev > 0.0:
-				if anim_player.current_animation != "roll1":
-					anim_player.play("roll1")
+				if _spinrev >= 6.0:
+					if anim_player.current_animation != "roll1":
+						anim_player.play("roll1")
+				elif anim_player.current_animation != "roll0":
+					anim_player.play("roll0")
 			elif anim_player.current_animation != "look_down":
 				anim_player.play("look_down")
 		elif anim_player.current_animation != "idle" and temp_idling < _PATIENCE:
@@ -159,7 +195,7 @@ func _play_roll_animation() -> void:
 		sprite.flip_h = false
 	elif sign(velocity.x) < 0.0:
 		sprite.flip_h = true
-	if anim_player.current_animation != "roll0" and abs(velocity.x) < _TOP_SPEED:
-		anim_player.play("roll0")
-	elif anim_player.current_animation != "roll1" and abs(velocity.x) >= _TOP_SPEED:
+	if abs(velocity.x) >= _TOP_SPEED and anim_player.current_animation != "roll1":
 		anim_player.play("roll1")
+	elif abs(velocity.x) < _TOP_SPEED and anim_player.current_animation != "roll0":
+		anim_player.play("roll0")
