ef2af93d7a02c848f95e1b6b52fe4ba8204907b2
Rodrigo Ponce <96100905+xHappyface@users.noreply.github.com>
2025-01-28 12:29:55 -0500

    title screen improved slightly. game is now replayable on game overs.

--- scripts/models/game.gd
+++ scripts/models/game.gd
@@ -3,19 +3,34 @@ extends Node
 @onready var main_menu: Control = $MainMenu
 @onready var level_manager: LevelManager = $LevelManager
 
-var _game_ready: bool = false
+@onready var _title_screen: TextureRect = $MainMenu/TitleScreen
+@onready var _press_start: Label = $MainMenu/PressStart
+
+var _title_finished: bool = false
 
 func _ready() -> void:
-	var tween: Tween = create_tween()
-	tween.tween_property(main_menu.find_child("TitleScreen"), "modulate", Color(1.0, 1.0, 1.0, 1.0), 2.25)
-	tween.tween_property(main_menu.find_child("PressStart"), "modulate", Color(1.0, 1.0, 1.0, 1.0), 1.0)
-	tween.tween_callback(_toggle_game_ready)
+	_title_screen.modulate = Color(1.0, 1.0, 1.0, 0.0)
+	_press_start.modulate = Color(1.0, 1.0, 1.0, 0.0)
+	show_main_menu()
 
 func _process(_delta: float) -> void:
-	if main_menu.visible and _game_ready:
+	if main_menu.visible and _title_finished:
 		if Input.is_action_just_pressed("game_start"):
-			main_menu.hide()
-			level_manager.start_level(level_manager.default_level)
+			hide_main_menu()
+			level_manager.start_level()
 
-func _toggle_game_ready() -> void:
-	_game_ready = not _game_ready
+func _toggle_title_finished() -> void:
+	_title_finished = not _title_finished
+
+func hide_main_menu() -> void:
+	main_menu.hide()
+	_title_screen.modulate = Color(1.0, 1.0, 1.0, 0.0)
+	_press_start.modulate = Color(1.0, 1.0, 1.0, 0.0)
+	_title_finished = false
+
+func show_main_menu() -> void:
+	var tween: Tween = create_tween()
+	main_menu.show()
+	tween.tween_property(_title_screen, "modulate", Color(1.0, 1.0, 1.0, 1.0), 2.25)
+	tween.tween_property(_press_start, "modulate", Color(1.0, 1.0, 1.0, 1.0), 1.0)
+	tween.tween_callback(_toggle_title_finished)
--- scripts/models/level_manager.gd
+++ scripts/models/level_manager.gd
@@ -13,6 +13,8 @@ var _player: Player = null
 const _STARTING_RINGS: int = 0
 const _STARTING_LIVES: int = 3
 
+var _is_level_running: bool = false
+
 var score: int = 0
 var rings: int = _STARTING_RINGS
 var lives: int = _STARTING_LIVES
@@ -21,14 +23,15 @@ func _ready() -> void:
 	pass
 
 func _process(_delta: float) -> void:
-	if (not _level) or (not _player):
+	if not _is_level_running:
 		return
 	_overlay.set_time(_time.wait_time - _time.time_left)
 
-func start_level(packed_level: PackedScene) -> void:
-	if (not packed_level.can_instantiate()) or _level:
+func start_level(packed_level: PackedScene = default_level) -> void:
+	if (not packed_level.can_instantiate()):
 		return
-	_level = packed_level.instantiate()
+	if not _level:
+		_level = packed_level.instantiate()
 	_game_view.add_child(_level)
 	_player = _level.player
 	_overlay.reparent(_level.camera)
@@ -40,11 +43,26 @@ func start_level(packed_level: PackedScene) -> void:
 	_player.game_over.connect(_on_game_over)
 	_overlay.show()
 	_game_view.get_parent().show()
-	_time.start()
+	_time.start(600.0)
+	_is_level_running = true
 
 func _on_game_over() -> void:
 	print("GAME OVER")
 	_player.active = false
+	_is_level_running = false
 	_time.stop()
 	_level.bg_music.stop()
 	_overlay.anim_player.play("game_over")
+	var tween: Tween = create_tween()
+	tween.tween_callback(get_parent().show_main_menu).set_delay(3.0)
+	tween.tween_callback(_game_view.get_parent().hide)
+	tween.tween_callback(_overlay.hide)
+	tween.tween_callback(_free_level)
+
+func _free_level() -> void:
+	if not _level:
+		return
+	_overlay.reparent(self)
+	_overlay.anim_player.play("RESET")
+	_level.queue_free()
+	_level = null
